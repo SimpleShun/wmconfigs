ea13c7ef2e5bf7f330b6db7d404b1b668c7b3958
ShunDesktop <simpleshun@archlinux.google.com>
2023-12-14 14:59:01 +0545

    newConfigs

--- waybar/scripts/changeaudiosource.sh
+++ waybar/scripts/changeaudiosource.sh
@@ -8,11 +8,11 @@ fi
 read a < $flag_file
 switch=$(( a % 2))
 if [[ "$switch" -eq 0 ]]; then
-	pactl set-sink-port alsa_output.pci-0000_00_1b.0.analog-stereo 'analog-output-headphones'
+	pactl set-sink-port @DEFAULT_SINK@ 'analog-output-headphones'
 	a=$((a+1))
 	echo $a > /tmp/seeinghowmanytimesipressedthisbutton.txt
 else
-	pactl set-sink-port alsa_output.pci-0000_00_1b.0.analog-stereo 'analog-output-lineout'
+	pactl set-sink-port @DEFAULT_SINK@ 'analog-output-lineout'
 	a=$((a+1))
 	echo $a>$flag_file
 fi
